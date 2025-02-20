8eb39d50bcd44c7499e50ac2e08ccfc3bc529850
psgwei <psgwei@protonmail.com>
2025-02-20 10:27:54 +0800

    search bar

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,10 +1,24 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -20,9 +34,8 @@ fun BookListScreenRoot(
         onAction = { action ->
             when (action) {
                 is BookListAction.OnBookClick -> onBookClick(action.book)
-                else -> Unit
+                else -> viewModel.onAction(action)
             }
-            viewModel.onAction(action)
         }
     )
 }
@@ -32,7 +45,16 @@ private fun BookListScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit,
 ) {
-
+    // null if software keyboard cannot be controller
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column(
+        modifier = Modifier.fillMaxSize().background(DarkBlue)
+            .padding(WindowInsets.systemBars.asPaddingValues())
+    ) {
+        BookSearchBar(state.searchQuery, { onAction(BookListAction.OnSearchQueryChange(it)) }, {
+            keyboardController?.hide()
+        }, modifier = Modifier.widthIn(400.dp).fillMaxWidth().padding(16.dp))
+    }
 }
 
 
